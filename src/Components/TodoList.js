62900c235d9d3698b1a2a5cb926b847bb09662d5
ferhat kaplan <fkmll2121@gmail.com>
2023-10-07 17:07:02 +0300

    Create TodoList component
    - Created a "TodoList" component.
    - This component displays a task list and supports marking tasks as complete and deleting them.
    - Users can mark a task as complete by clicking the "Complete" button next to each task.
    -  Additionally, users can delete a task from the list by clicking the "Delete" button next to it.
    - This component will be used to support task management features in the project.

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-function TodoList() {
-    return <div>TodoList</div>;
+function TodoList({ todos, completeTodo, deleteTodo }) {
+    return (
+        <div>
+            {todos.map((todo, index) => (
+                <div key={index}>
+                    {todo.text}
+                    <button onClick={() => completeTodo(index)}>Tamamla</button>
+                    <button onClick={() => deleteTodo(index)}>Sil</button>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default TodoList;
