824b7d868ead16e99b74f3ebac0ee73b651dde02
ferhat kaplan <fkmll2121@gmail.com>
2023-10-07 17:30:47 +0300

    Create TodoForm component for adding new tasks
    - Implemented a TodoForm component that allows users to input and add new tasks to the list.
    - Users can enter a task description in the input field and click the 'Ekle' (Add) button to add the task to the list.
    - The component ensures that empty tasks are not added to the list.

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-function TodoForm() {
-    return <div>TodoForm</div>;
+function TodoForm({ addNewTodo }) {
+    const [text, setText] = useState("");
+
+    const handleAddTodo = () => {
+        if (text.trim() !== "") {
+            addNewTodo(text);
+            setText("");
+        }
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Yeni bir gorev ekleyin"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+
+            <button onClick={handleAddTodo}>Ekle</button>
+        </div>
+    );
 }
 
 export default TodoForm;
